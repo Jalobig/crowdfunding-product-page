8db47e5c241e231c76d1eadedcacbdf09253e75e
Jalobig <alexis.jason7@gmail.com>
2023-09-15 09:26:29 -0400

    mobile design completed

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -3,12 +3,13 @@ import ImageHeroDesktop from '../../images/image-hero-desktop.jpg'
 import ImageHeroMobile from '../../images/image-hero-mobile.jpg'
 import classes from './Header.module.scss'
 import Navbar from '../../components/Navbar/Navbar';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
 const Header = () => {
-
+const media = useMediaQuery('only screen and (max-width:479px)')
   return (
     <header className={classes.header}>
-      <img src={ImageHeroDesktop} alt='Background Hero' className={classes.header__img}/>
+      <img src={media? ImageHeroMobile:ImageHeroDesktop} alt='Background Hero' className={classes.header__img}/>
       <Navbar/>
     </header>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import classes from './Navbar.module.scss'
-import Logo from '../../images/logo.svg'
+import React, { useState } from "react";
+import classes from "./Navbar.module.scss";
+import Logo from "../../images/logo.svg";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import IconMenu from "../../images/icon-hamburger.svg";
+import IconClose from "../../images/icon-close-menu.svg";
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+  const media = useMediaQuery("only screen and (max-width:479px)");
+  const menuHandler = () => {
+    setIsActive((prevState) => !prevState);
+  };
   return (
-    <nav className={classes.navbar}>
-        <img src={Logo} alt='Logo of crowdfund' className={classes.navbar__logo}/>
-        <ul className={classes.navbar__nav}>
-        <li className={classes['navbar__nav--item']}>About</li>
-        <li className={classes['navbar__nav--item']}>Discover</li>
-        <li className={classes['navbar__nav--item']}>Get Started</li>
+    <>
+    {media && isActive && <div className={classes.backdrop}></div>}
+      <nav className={classes.navbar}>
+        <img
+          src={Logo}
+          alt="Logo of crowdfund"
+          className={classes.navbar__logo}
+        />
+        {media && (
+          <img
+            src={isActive ? IconClose : IconMenu}
+            alt={`${isActive ? "Close" : "Menu"} Icon`}
+            className={classes.navbar__icon}
+            onClick={menuHandler}
+          />
+        )}
+        {(!media || isActive) && (
+          <ul className={classes.navbar__nav}>
+            <li className={classes["navbar__nav--item"]}>About</li>
+            <li className={classes["navbar__nav--item"]}>Discover</li>
+            <li className={classes["navbar__nav--item"]}>Get Started</li>
+          </ul>
+        )}
+      </nav>
+    </>
+  );
+};
 
-        </ul>
-    </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
